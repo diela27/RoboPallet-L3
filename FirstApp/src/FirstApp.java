f904bc4f0413bb4170157cc2a92f5b72bb4488ff
Utilisateur <Utilisateur@10.3.234.82>
2026-09-14 15:59:48 +0200

    Initial commit

---
+++ FirstApp/src/FirstApp.java
@@ -0,0 +1,20 @@
+import lejos.hardware.BrickFinder;
+import lejos.hardware.lcd.GraphicsLCD;
+import lejos.utility.Delay;
+
+
+public class FirstApp {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        GraphicsLCD g = BrickFinder.getDefault().getGraphicsLCD();
+        
+        g.drawString("Hello World", 0, 0, GraphicsLCD.VCENTER |
+                GraphicsLCD.LEFT);
+        
+        Delay.msDelay(5000);
+    }
+
+}
